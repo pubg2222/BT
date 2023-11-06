7061dc4f93658c1b8917a2060c10431d37b80610
pubg2222 <118238111+pubg2222@users.noreply.github.com>
2023-11-06 17:29:47 +0530

    Update studData.sol

--- studData.sol
+++ studData.sol
@@ -5,32 +5,24 @@ contract MarksManagmtSys{
         uint ID;
         string fName;
         string lName;
-        uint marks;
+        uint class;
     }
     address owner;
     
     uint public stdCount = 0;
-    event log(string, uint);
+    // event log(string, uint);
     StudentStruct[] public stdRecords;
     
     constructor() {
         owner=msg.sender;
     }
 
-    function addNewRecords(uint _ID,string memory _fName,string memory _lName,uint _marks) public payable {
+    function addNewRecords(uint _ID,string memory _fName,string memory _lName,uint _class) public {
         stdCount += 1;
-        stdRecords.push(StudentStruct(_ID , _fName , _lName , _marks));
-    }
-
-    fallback() external payable { 
-        emit log("Fallback", gasleft());
+        stdRecords.push(StudentStruct(_ID , _fName , _lName , _class));
     }
 
     receive() external payable {
-        emit log("receive", gasleft());
-    }
-
-    function getAllRecords() public view returns(StudentStruct[] memory) {
-        return stdRecords;
+        revert();
     }
 }
